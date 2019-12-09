d6920c32e1d61a464b7d473a535114695dac904e
Matias Trucido <matiastrucido1234@gmail.com>
2019-12-09 20:04:55 -0300

    Create index.js

---
+++ services/index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const jwt = require('jwt-simple')
+const moment = require('moment')
+const config = require('../config')
+
+function createToken (user) {
+  const payload = {
+    sub: user.uid,
+    iat: moment().unix(),
+    exp: moment().add(config.DAYS_TOKEN_EXP, 'days').unix()
+  }
+  return jwt.encode(payload, config.SECRET_TOKEN)
+}
+
+function decodeToken (token) {
+  const decoded = new Promise((resolve, reject) => {
+    try {
+      const payload = jwt.decode(token, config.SECRET_TOKEN)
+      if (payload.exp <= moment().unix()) {
+        reject({
+          status: 401,
+          message: 'El token ha expirado'
+        })
+      }
+      resolve(payload.sub)
+    } catch (err) {
+      reject({
+        status: 500,
+        message: 'Token invalido'
+      })
+    }
+  })
+  return decoded
+}
+
+module.exports = {
+  createToken,
+  decodeToken
+}
